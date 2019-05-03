0ceb030d02036da226222a4c03dbaf26dd7d2101
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2019-05-03 20:35:24 +0530

    Added indentity context and identity user

---
+++ Sports/Data/SportsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sports.Models;
+
+namespace Sports.Data
+{
+
+
+    public class SportsContext :IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public SportsContext
+           (DbContextOptions<SportsContext> options)
+            : base(options)
+        {
+            //nothing here
+        }
+
+    }
+}
